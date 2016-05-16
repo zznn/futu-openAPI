2a0d405c00d7d9ba95723f94ded68c99fd3828fc
futuopen <futuopen@qq.com>
2016-05-16 14:45:08 +0800

    Update mainapp.py

--- app/mainapp.py
+++ app/mainapp.py
@@ -42,7 +42,7 @@ def trade_token():
 	appid = request.json['appid']
 	cc = check_parameters(request.json)
 	message = cc.get_trade_token(trade_pswd)
-	if 'ClientWarning' in message:
+	if message['result_code'] != 0 and message['error_msg' == 'didn\'t get accesstoken']:
 		no_db_logger.info('didn\'t get accesstoken')
 		return json.dumps({'result_code':2,'error_msg':'didn\'t get accesstoken'}, ensure_ascii=False)
 	if message['result_code'] == 0:
